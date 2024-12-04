a13f7d6e85ae963a05465700231e916298f190b6
lazarevtill <lazarevtill@gmail.com>
2024-12-04 17:57:06 +0100

    4

--- frontend/polly-system/src/pages/Dashboard.tsx
+++ frontend/polly-system/src/pages/Dashboard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useQuery } from 'react-query';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Alert, AlertDescription } from '@/components/ui/alert';
